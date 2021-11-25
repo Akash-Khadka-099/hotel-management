81bcc323dac2e99bffaec807f85f98ead391c2e1
Akash Khadka <akashkhadka099@gmail.com>
2021-11-25 22:03:06 +0545

    this is a first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Resturant from './components/Basics/Resturant'
+
+export default function App() {
+  return (
+    <>
+      <Resturant />
+      
+    </>
+  )
+}
